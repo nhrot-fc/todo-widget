93ca33dbdbbaa930199f49175f5d85b965186cd2
nhrot-fc <a20213812@pucp.edu.pe>
2026-01-03 01:56:19 -0500

    upd app_name to todowidget

--- src/core/config.py
+++ src/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     # Application
-    app_name: str = "TodoWidget"
+    app_name: str = "todowidget"
     storage_path: str = "tasks.json"
 
     # Logging
